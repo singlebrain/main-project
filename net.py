e13f91c376528259579ce59feb6960aec46acb30
singlebrain <ashik.alias@gmail.com>
2017-12-26 19:55:28 +0530

    percept

---
+++ net.py
@@ -0,0 +1,69 @@
+import csv
+import numpy as np
+from keras.models import Sequential
+from keras.models import Model
+from keras.layers import Dense, Dropout, Activation
+from keras.optimizers import SGD
+# Generate dummy data
+x_train = np.random.random((10, 20))
+y_train = np.random.randint(2, size=(10, 10))
+x_test = np.random.random((100, 20))
+y_test = np.random.randint(2, size=(100, 10))
+
+model = Sequential()
+model.add(Dense(512, input_dim=20, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='sigmoid'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='binary_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy'])
+x_tra = []
+y_tra=[]
+xtr=[]
+ytr=[]
+csvFile = open("sp.csv", "rb")
+csvReader = csv.reader(csvFile)
+i=0
+for row in csvReader:
+	x_tra=row[0:20]
+	y_tra=row[20:30]
+	xtr.append(x_tra)
+	ytr.append(y_tra)
+x=[10,20,30,40,50,60,70,80,90,100,1,2,3,4,5,6,7,8,9,10]
+model.fit(xtr, ytr,epochs=200,batch_size=128)
+score = model.evaluate(x_test, y_test, batch_size=128)
+xtet=[]
+xtet.append(x)
+ytr=model.predict_proba(np.array(xtet))
+print ytr
+
