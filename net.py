ca5a68fc6e876b65e2c13cf3c9dcc9fdd2ef3e40
singlebrain <ashik.alias@gmail.com>
2018-01-01 08:43:47 +0530

    net modified

--- net.py
+++ net.py
@@ -12,41 +12,53 @@ y_test = np.random.randint(2, size=(100, 10))
 
 model = Sequential()
 model.add(Dense(512, input_dim=20, activation='relu'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(10, activation='sigmoid'))
+#model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
 
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='binary_crossentropy',
-              optimizer=sgd,
+              optimizer='adam',
               metrics=['accuracy'])
+'''
+model.add(Dense(20, input_dim=5, init='uniform', activation='tanh'))
+model.add(Dense(1, init='uniform', activation='linear'))
+
+# Compile model
+model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+
+# Fit the model
+model.fit(X1, Y1, nb_epoch=5000, batch_size=10,  verbose=2)
+numpy.savetxt("trainresults.csv", PredTestSet, delimiter=",")
+numpy.savetxt("valresults.csv", PredValSet, delimiter=",")
+'''
 x_tra = []
 y_tra=[]
 xtr=[]
@@ -60,7 +72,7 @@ for row in csvReader:
 	xtr.append(x_tra)
 	ytr.append(y_tra)
 x=[10,20,30,40,50,60,70,80,90,100,1,2,3,4,5,6,7,8,9,10]
-model.fit(xtr, ytr,epochs=200,batch_size=128)
+model.fit(xtr, ytr,epochs=20,batch_size=1)
 score = model.evaluate(x_test, y_test, batch_size=128)
 xtet=[]
 xtet.append(x)
