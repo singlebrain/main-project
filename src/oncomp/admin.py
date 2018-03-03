cc887ee09664714a7ce268c80c8a44934bf84521
singlebrain <ashik.alias@gmail.com>
2018-03-03 20:25:43 +0530

    delting

--- src/oncomp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import *
-from django.apps import apps
-# Register your models here.
-for model in apps.get_app_config('oncomp').models.values():
-    admin.site.register(model)
--- src/oncomp/compiler/albinpaul.ks@gmail.com/serenity.py
+++
@@ -1,4 +0,0 @@
-a =[ ]
-for i in range(2):
-    a.append(raw_input())
-print (int(a[0])+int(a[1]))
